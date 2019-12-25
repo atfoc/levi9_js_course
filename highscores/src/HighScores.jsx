5a4e2f8abffe070dae44cc8ff7fd56ad65fe0483
Pedja Trifunov <trifunov.pedja@gmail.com>
2019-12-25 16:28:49 +0100

    Added react part of the project for showing leader board

---
+++ highscores/src/HighScores.jsx
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from 'react';
+import {isElementOfType} from "react-dom/test-utils";
+import {Table} from "./Table";
+
+
+export function HighScores(props)
+{
+    const [highscores, setHighScores] = useState(null);
+
+    const url = (props.url !== null && props.url !== undefined) ? props.url : 'http://localhost:3001/highScores';
+    useEffect(()=> {
+        fetch(url)
+            .then(response=>{
+                return response.json();
+            })
+            .then(response => {
+                const headers = ['Nickname', 'Score'];
+                const tableBody = response.map((item)=>{
+                    return [item.nickname, item.score];
+                });
+
+                setHighScores([headers, ...tableBody]);
+            })
+    }, [url]);
+
+    if(highscores === null)
+    {
+        return <h1>Loading...</h1>;
+    }
+    else
+    {
+        return <Table data={highscores}/>
+    }
+
+
+    return <h1>Test</h1>
+}
